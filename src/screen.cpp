ce351789260f8de6277e8bc77e737d28292b7886
Armin <hellospencer@gmail.com>
2016-02-24 12:17:10 -0800

    added screen

---
+++ src/screen.cpp
@@ -0,0 +1,24 @@
+/*
+ * screen.cpp
+ *
+ *  Created on: Feb 24, 2016
+ *      Author: armin
+ */
+
+
+#include "screen.hpp"
+
+
+Screen::Screen()  {
+
+}
+
+Screen::~Screen()  {
+
+}
+
+
+void Screen::set(int x, int y, char c, int color) {
+
+}
+
---
+++ src/screen.hpp
@@ -0,0 +1,20 @@
+/*
+ * screen.hpp
+ *
+ *  Created on: Feb 24, 2016
+ *      Author: armin
+ */
+
+#ifndef SCREEN_HPP_
+#define SCREEN_HPP_
+
+class Screen {
+public:
+	Screen();
+	~Screen();
+	void set(int x, int y, char c, int color);
+};
+
+
+
+#endif /* SCREEN_HPP_ */
