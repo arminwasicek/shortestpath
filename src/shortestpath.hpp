cc53b4c852f97307dea3730e3916fa9ff3e498ea
Armin <hellospencer@gmail.com>
2016-02-27 13:58:01 -0800

    added more doc

--- src/shortestpath.hpp
+++ src/shortestpath.hpp
@@ -19,26 +19,41 @@
  */
 typedef priority_queue<WeightedPoint2D,vector<WeightedPoint2D>, WeightedPoint2D::compare> weightedpointpq;
 
+/*! \brief Generic class that implements the data structures and access methods for the shortest path search.
+ *
+ * Subclass and overwrite the runbfs method with a specific algorithm.
+ */
 class ShortestPath {
 protected:
-	vector<Point2D> frontier_vec;
-	vector<Point2D> visited;
-	map<Point2D, Point2D> camefrom;
-	map<Point2D, int> costsofar;
-	weightedpointpq frontier_pq;
-	Grid *grid = nullptr;
-	Screen *screen = nullptr;
+	vector<Point2D> frontier_vec;		/*!< A vector to store points for representing the current frontier */
+	vector<Point2D> visited;			/*!< A vector to store the points visited. */
+	map<Point2D, Point2D> camefrom;		/*!< A map to track the history of the algorithm */
+	map<Point2D, int> costsofar;		/*!< A map that stores the costs incurred so far for a point */
+	weightedpointpq frontier_pq;		/*!< A priority queue to store points for representing the current frontier */
+	Grid *grid = nullptr;				/*!< The grid representing the context in that the algorithm works */
+	Screen *screen = nullptr;			/*!< The screen abstracts the IO operations for the algorithm. */
 
+	/*! \brief Marks a point as visited in #frontier_vec. */
 	void visit(Point2D p);
+	/*! \brief Checks, if a point has been visited in #visited. */
 	bool isvisited(Point2D p);
+	/*! \brief Checks, if a point has been visited in #camefrom. */
 	bool incamefrom(Point2D p);
+	/*! \brief Checks, if a point has been assigned a cost in #costsofar . */
 	bool incostsofar(Point2D p);
+	/*! \brief Checks, if a point has been assigned a cost in #costsofar . */
 	void visitfrom(Point2D curr, Point2D from);
+	/*! \brief Checks, if a point has been visited in #frontier_vec. */
 	bool infrontier(Point2D p);
+	/*! \brief Outputs a vector using cout. */
 	void print_vec(vector<Point2D> v);
+	/*! \brief Outputs a map using cout. */
 	void print_map(map<Point2D, Point2D> v);
+	/*! \brief Outputs a priority queue using cout. */
 	void print_pq(weightedpointpq v);
+	/*! \brief Plots the points in the priority queue using ncurses. */
 	void plot_pq(weightedpointpq v);
+	/*! \brief Free all resources and empty all data structures. */
 	void cleanup();
 
 	/*! \brief Outputs the current state of the frontier vector.
